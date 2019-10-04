034f22c91aba55cbbad4ae80b127fe68861cc403
MyleneSimon <mylene.simon@nist.gov>
2019-10-04 10:53:05 -0400

    feat: add argo service patch to deploy.sh

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+kubectl patch svc argo-ui -n argo -p "$(cat argo-service-patch.yaml)"
 kubectl create -f mongo-deployment.yaml
 kubectl create -f mongo-service.yaml
 kubectl create -f backend-deployment.yaml
